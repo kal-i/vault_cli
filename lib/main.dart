7a6ef975fc3ce621fd0178e4c4be4a2cf7696439
kali <paul.maceres00@gmail.com>
2025-09-22 20:14:35 +0800

    refactor(main): invoke ScreenshotService, convert to MultiBlocProvider, and set SplashScreenView as home

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'config/theme/app_theme.dart';
+import 'core/services/screenshot_service.dart';
 import 'features/auth/presentation/bloc/vault_auth_bloc.dart';
-import 'features/auth/presentation/views/vault_auth_view.dart';
 
-
+import 'features/startup/presentation/bloc/startup_bloc.dart';
+import 'features/startup/presentation/views/splash_screen_view.dart';
 import 'init_dependencies.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDependencies();
-  runApp(const VaultCLIApp());
+  await ScreenshotService.enable();
+  runApp(const VaultCliApp());
 }
 
-class VaultCLIApp extends StatelessWidget {
-  const VaultCLIApp({super.key});
+class VaultCliApp extends StatelessWidget {
+  const VaultCliApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => serviceLocator<VaultAuthBloc>(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => serviceLocator<StartupBloc>()..add(NavigateToEvent()),
+        ),
+        BlocProvider(create: (_) => serviceLocator<VaultAuthBloc>()),
+      ],
       child: MaterialApp(
         title: 'Vault CLI',
         debugShowCheckedModeBanner: false,
         theme: AppTheme.base,
-        home: const VaultAuthView(),
+        home: const SplashScreenView(),
       ),
     );
   }
