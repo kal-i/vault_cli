a742ac25f030988a2ad32aa144f54a0cf23bf071
kali <paul.maceres00@gmail.com>
2025-09-22 13:58:58 +0800

    feat(android): first platform-specific code - disable/enable screenshots

--- android/app/src/main/kotlin/com/example/vault_cli/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/vault_cli/MainActivity.kt
@@ -1,5 +1,32 @@
 package com.example.vault_cli
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.view.WindowManager
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val CHANNEL = "screenshot_protection";
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "enable" -> {
+                    window.setFlags(
+                        WindowManager.LayoutParams.FLAG_SECURE,
+                        WindowManager.LayoutParams.FLAG_SECURE
+                    )
+                    result.success(true)
+                }
+                "disable" -> {
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+                    result.success(null)
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
+}
