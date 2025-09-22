3f56ad08c0e6d9bf4946087a8e5d7d733b83db16
kali <paul.maceres00@gmail.com>
2025-09-22 19:30:29 +0800

    remove toString() method

--- lib/core/errors/unknown_failure.dart
+++ lib/core/errors/unknown_failure.dart
@@ -2,7 +2,4 @@ import 'failure.dart';
 
 class UnknownFailure extends Failure {
   const UnknownFailure({required super.message});
-
-  @override
-  String toString() => 'Unknown error: $message';
 }
