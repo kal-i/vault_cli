6f3d6ddb6b676f447d86f02ed4584c08f40cf615
kali <paul.maceres00@gmail.com>
2025-09-24 20:40:43 +0800

    refactor(main): Add splash screen as the default home route

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'config/theme/app_theme.dart';
 import 'core/services/screenshot_service.dart';
 import 'features/auth/presentation/bloc/vault_auth_bloc.dart';
+import 'features/splash/presentation/views/splash_screen_view.dart';
 
-import 'features/startup/presentation/bloc/startup_bloc.dart';
-import 'features/startup/presentation/views/splash_screen_view.dart';
 import 'init_dependencies.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await initializeDependencies();
   await ScreenshotService.enable();
+  FlutterNativeSplash.remove();
   runApp(const VaultCliApp());
 }
 
@@ -23,9 +25,6 @@ class VaultCliApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(
-          create: (_) => serviceLocator<StartupBloc>()..add(NavigateToEvent()),
-        ),
         BlocProvider(create: (_) => serviceLocator<VaultAuthBloc>()),
       ],
       child: MaterialApp(
