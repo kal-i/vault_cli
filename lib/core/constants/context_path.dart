ed6032bf4453e785789a615f8e2e7ac0d859e6b0
kali <paul.maceres00@gmail.com>
2025-09-24 13:19:20 +0800

    Add ContextPath

---
+++ lib/core/constants/context_path.dart
@@ -0,0 +1,6 @@
+class ContextPath {
+  static const auth = 'auth>';
+  static const recovery = 'auth/recovery>';
+  static const recoverySetup = 'auth/recovery/setup>';
+  static const vault = 'vault>';
+}
